11533b5bcf4f71611f6041a8cc3d6917b7cd7b1c
Patrick Collins <pat@burned.com>
2026-01-25 16:24:57 -0500

    migrate curio to keyboard.json format
    Combined info.json, config.h, rules.mk into keyboard.json.
    Removed LAYOUT_all macro from curio.h (auto-generated from keyboard.json).

--- firmware/curio/curio.h
+++ firmware/curio/curio.h
@@ -17,25 +17,3 @@
 #pragma once
 
 #include "quantum.h"
-
-/* This is a shortcut to help you visually see your layout.
- *
- * The first section contains all of the arguments representing the physical
- * layout of the board and position of the keys.
- *
- * The second converts the arguments into a two-dimensional array which
- * represents the switch matrix.
- */
-
-#define LAYOUT_all( \
-    K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, \
-    K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110,       K112, \
-    K200,       K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, K212, \
-    K300, K301, K302,       K304, K305, K306, K307, K308,       K310, K311, K312  \
-) \
-{ \
-    { K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011,  K012 }, \
-    { K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  KC_NO, K112 }, \
-    { K200,  KC_NO, K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211,  K212 }, \
-    { K300,  K301,  K302,  KC_NO, K304,  K305,  K306,  K307,  K308,  KC_NO, K310,  K311,  K312 }  \
-}
